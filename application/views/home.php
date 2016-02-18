dc01662a89ec3ed444247abecf6234df4ec63584
cybersteel8 <cybersteel8@users.noreply.github.com>
2016-02-19 03:06:34 +1100

    Added Bootstrap resources - made dummy home.php layout for working with

---
+++ application/views/home.php
@@ -0,0 +1,18 @@
+<?php defined('BASEPATH') OR exit('No direct script access allowed'); ?>
+<!DOCTYPE html>
+<html>
+	<head>
+		<link rel="stylesheet" href="resources/css/bootstrap.css" />
+		<link rel="stylesheet" href="resources/css/style.css" />
+		<meta name="viewport" content="width=device-width, initial-scale=1">
+	</head>
+	<body>
+		<div class="jumbotron">
+			<div class="container">
+				<div class="row">
+					<div class="col-md-3"><p>dummy</p></div>
+				</div>
+			</div>
+		</div>
+	</body>
+</html>

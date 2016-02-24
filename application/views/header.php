3e45deadd19b5e9305cade28d637b762f9822b57
cybersteel8 <cybersteel8@users.noreply.github.com>
2016-02-24 22:15:15 +1100

    Removed bootstrap dependency / modularized PHP files-controllers

---
+++ application/views/header.php
@@ -0,0 +1,11 @@
+<?php defined('BASEPATH') OR exit('No direct script access allowed'); ?>
+<!DOCTYPE html>
+<html>
+<head>
+	<link rel="stylesheet" href="assets/css/style.css" />
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+</head>
+<header>
+
+</header>
+<body>

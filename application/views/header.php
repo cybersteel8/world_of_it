55d7ed489ff8d4bcd838383d563789054d3c080d
cybersteel8 <cybersteel8@users.noreply.github.com>
2016-03-18 00:03:06 +1100

    building header.php

--- application/views/header.php
+++ application/views/header.php
@@ -5,6 +5,8 @@
 	<link rel="stylesheet" href="<?php echo 'http://' . $_SERVER['SERVER_NAME'] . '/world_of_it/assets/css/style.css' ?>"/>
 </head>
 <header>
-
+	<div class="container">
+		<h1 id="mainHeading">WORLD OF I.T</h1>
+	</div>
 </header>
 <body>

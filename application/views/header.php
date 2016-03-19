5226af470747fa405aa44b7985cb090b684a6318
cybersteel8 <simon_elturk@hotmail.com>
2016-03-19 20:07:17 +1100

    website header nearly complete

--- application/views/header.php
+++ application/views/header.php
@@ -7,6 +7,17 @@
 <header>
 	<div class="container">
 		<h1 id="mainHeading">WORLD OF I.T</h1>
+		<nav>
+			<ul>
+				<li><a href=""><p>HOME</p></a></li>
+				<li><a href=""><p>PROJECTS</p></a></li>
+				<li><a href=""><p>SERVICES</p></a></li>
+				<li><a href=""><p>ABOUT</p></a></li>
+			</ul>
+		</nav>
+		<p id="logIn">LOG IN</p>
+		<p id="or">OR</p>
+		<p id="startNow">START NOW</p>
 	</div>
 </header>
 <body>

f56ee0af15609c28f19c2191c13a63e3808312a4
cybersteel8 <cybersteel8@users.noreply.github.com>
2016-03-19 18:44:24 +1100

    built all the divs with backgrounds for home.php

--- application/views/home.php
+++ application/views/home.php
@@ -1,3 +1,17 @@
-<div id="main">
+<div id="container">
+    <div id="carousel">
 
+    </div>
+
+    <div id="callToAction1">
+
+    </div>
+
+    <div id="positives">
+
+    </div>
+
+    <div id="packages">
+
+    </div>
 </div>

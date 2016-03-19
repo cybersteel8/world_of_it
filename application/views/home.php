9012b93f2b762b16b350f2f0aa1a330974b504ac
cybersteel8 <simon_elturk@hotmail.com>
2016-03-19 21:33:58 +1100

    positives and packages both set up

--- application/views/home.php
+++ application/views/home.php
@@ -4,7 +4,7 @@
 	</div>
 
 	<div id="callToAction1">
-		<div style="padding: 12px 0px 0px 500px;">
+		<div style="padding: 12px 0px 0px 25%;">
 			<p style="color: #e5e5e5; font-size: 34px; float: left;">START A PROJECT WITH US TODAY.</p>
 			<p style="color: #ec5e46; font-size: 34px; float: left; margin-left: 20px;">CHOOSE A PACKAGE THAT SUITS YOU BEST.</p>
 			<a href=""><p id="boxButton" style="font-size: 30px; float: left; margin: -4px 0px 0px 20px;">START NOW</p></a>
@@ -12,10 +12,21 @@
 	</div>
 
 	<div id="positives">
+		<div style="
+			border-left: 1px solid #e5e5e5;
+			border-right: 1px solid #e5e5e5;
+			width: 400px;
+			height: inherit;
+			margin: 0 auto;">
 
+		</div>
 	</div>
 
 	<div id="packages">
-
+		<div style="padding: 40px 0px 0px 25%">
+			<div id="pkg"></div>
+			<div id="pkg"></div>
+			<div id="pkg"></div>
+		</div>
 	</div>
 </div>

5bf2bf21f0f2eeafbe27891db1a9f51b3e5968c5
cybersteel8 <simon_elturk@hotmail.com>
2016-03-23 19:52:08 +1100

    button and link animations; complete footer; fixed more code to be collapsable

--- application/views/header.php
+++ application/views/header.php
@@ -9,16 +9,16 @@
 		<p id="mainHeading">WORLD OF I.T</p>
 		<nav>
 			<ul>
-				<li><a href=""><p>HOME</p></a></li>
-				<li><a href=""><p>PROJECTS</p></a></li>
-				<li><a href=""><p>SERVICES</p></a></li>
-				<li><a href=""><p>ABOUT</p></a></li>
+				<li><a href=""><p class="hvr-underline-from-left">HOME</p></a></li>
+				<li><a href=""><p class="hvr-underline-from-left">PROJECTS</p></a></li>
+				<li><a href=""><p class="hvr-underline-from-left">SERVICES</p></a></li>
+				<li><a href=""><p class="hvr-underline-from-left">ABOUT</p></a></li>
 			</ul>
 		</nav>
-		<div id="loginarea">
-			<a href=""><p class="boxButton" id="logIn">LOG IN</p></a>
+		<div id="login-wrapper">
+			<a href=""><p class="boxButton hvr-radial-out-white" id="logIn">LOG IN</p></a>
 			<p id="or">OR</p>
-			<a href=""><p class="boxButton">START NOW</p></a>
+			<a href=""><p class="boxButton hvr-radial-out-blue">START NOW</p></a>
 		</div>
 	</div>
 </header>
--- application/views/home.php
+++ application/views/home.php
@@ -3,11 +3,11 @@
 
 	</div>
 
-	<div id="callToAction1">
+	<div id="callToAction">
 		<div class="container">
-			<p class="cta-heading" style="color: #e5e5e5;">START A PROJECT WITH US TODAY.</p>
-			<p class="cta-heading" style="color: #ec5e46; margin-left: 20px;" >CHOOSE A PACKAGE THAT SUITS YOU BEST.</p>
-			<a href=""><p class="boxButton" id="startNow-CTA">START NOW</p></a>
+			<p class="cta-text" style="color: #e5e5e5;">START A PROJECT WITH US TODAY.</p>
+			<p class="cta-text" style="color: #ec5e46; margin-left: 20px;" >CHOOSE A PACKAGE THAT SUITS YOU BEST.</p>
+			<a href=""><p class="boxButton hvr-radial-out-blue" id="startNow-CTA">START NOW</p></a>
 		</div>
 	</div>
 
@@ -20,6 +20,6 @@
 	<div id="packages-section">
 		<div class="pkg-item" style="height: 600px;"></div>
 		<div class="pkg-item" style="height: 600px;"></div>
-		<div class="pkg-item" style="height: 600px;"></div>
+		<div class="pkg-item" style="height: 600px; margin-right: 0;"></div>
 	</div>
 </main>
